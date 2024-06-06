ae7f1c0dbfe7a7ca9a385d40b03d10b6853aecc9
hexavhobia <rvx9forces@gmail.com>
2024-06-06 21:23:32 +0700

    feat: integrasi link berita related

--- src/features/berita/berita-related.tsx
+++ src/features/berita/berita-related.tsx
@@ -1,8 +1,11 @@
 import Loading from '@/components/Loading'
 import { Card3 } from '@/components/card/card-3'
 import { RelatedType } from '@/libs/types/beranda-type'
+import { setStateHalaman } from '@/store/reducer/stateIdHalaman'
 import { useGetBeritaDetailRelatedQuery } from '@/store/slices/berandaAPI'
 import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export function BeritaRelated({ id }: { id: string }) {
   // --- Berita Detail Page ---
@@ -24,6 +27,8 @@ export function BeritaRelated({ id }: { id: string }) {
     }
   }, [beritaDetailData?.related, id])
 
+  const dispatch = useDispatch()
+
   return (
     <div className="flex w-3/5 flex-col gap-12 phones:w-full">
       <div className="border-danger-700 flex border-b-4 pb-8">
@@ -37,13 +42,19 @@ export function BeritaRelated({ id }: { id: string }) {
       ) : (
         <div className="flex flex-col">
           {beritaDetailRelated?.map((item, idx) => (
-            <div key={idx}>
+            <Link
+              to={`/berita?page=${item?.seo}`}
+              onClick={() => {
+                dispatch(setStateHalaman({ id: item?.id, page: item?.seo }))
+              }}
+              key={idx}
+            >
               <Card3
                 judul={item?.judul}
                 hits={item?.hits}
                 tanggal={item?.tanggal}
               />
-            </div>
+            </Link>
           ))}
         </div>
       )}
