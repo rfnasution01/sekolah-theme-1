19ed15971ad526b6a29846ec7a154cc5d6551303
hexavhobia <rvx9forces@gmail.com>
2024-06-07 20:35:58 +0700

    feat: integrasi profil

--- src/pages/profil/index.tsx
+++ src/pages/profil/index.tsx
@@ -1,6 +1,14 @@
 import Loading from '@/components/Loading'
-import { ProfilBg } from '@/features/profil'
+import {
+  ProfilBg,
+  ProfilHasil,
+  ProfilSasaran,
+  ProfilTujuan,
+  ProfilVisi,
+} from '@/features/profil'
+import { IdentitasType } from '@/libs/types/beranda-type'
 import { ProfilType } from '@/libs/types/profil-type'
+import { useGetIdentitasQuery } from '@/store/slices/berandaAPI'
 import { useGetProfilQuery } from '@/store/slices/profilAPI'
 import { useEffect, useState } from 'react'
 
@@ -17,20 +25,34 @@ export default function ProfilPage() {
     }
   }, [data?.data])
 
+  // --- Identitas ---
+  const [identitas, setIdentitas] = useState<IdentitasType>()
+  const { data: identitasData } = useGetIdentitasQuery()
+
+  useEffect(() => {
+    if (identitasData?.data) {
+      setIdentitas(identitasData?.data)
+    }
+  }, [identitasData?.data])
+
   return (
-    <div className="mb-80 flex flex-col gap-32">
+    <div className="mb-32 flex flex-col gap-32">
       {/* --- Banner --- */}
       {loadingProfil ? (
         <Loading />
       ) : (
-        <div className="flex flex-col gap-32 bg-red-300">
-          <div className="h-[85vh] w-full">
+        <div className="flex flex-col gap-32">
+          <div className="flex h-[85vh] w-full flex-col gap-32">
             <ProfilBg
               profil={profil}
               height="h-[85vh]"
               gambar="/img/identitas.png"
             />
           </div>
+          <ProfilVisi profil={profil} />
+          <ProfilTujuan profil={profil} sekolah={identitas?.nama_website} />
+          <ProfilSasaran profil={profil} sekolah={identitas?.nama_website} />
+          <ProfilHasil profil={profil} sekolah={identitas?.nama_website} />
         </div>
       )}
     </div>
